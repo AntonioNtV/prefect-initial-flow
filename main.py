85444bab28a73fa74ca187510da731b3871f6f2f
Antonio Bertino <antonio.bertino.neto@ccc.ufcg.edu.br>
2021-10-07 09:35:15 -0300

    Feature(Logger): Logging tasks result

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import csv
 
+#from prefect import tasks
+from logger import logger
+
 def extract(path):
     with open(path, "r") as file:
         text = file.readline().strip()
     data = [int(i) for i in text.split(",")]
+
+    logger("extracted data: {data}".format(data = data))
     return data
 
 def transform(data):
     t_data = [i + 1 for i in data]
+    logger("transformed data: {data}".format(data = t_data))
     return t_data
 
 def load(data, path):
